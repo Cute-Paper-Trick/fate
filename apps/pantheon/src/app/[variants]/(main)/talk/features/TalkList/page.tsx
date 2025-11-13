f86acf822bba6218cfd74ad831a66515817d997b
xiu.ming <code1bate@sina.com>
2025-11-13 09:59:46 +0000

    feat: 修复交流中心内容不全 (#28)
    修复交流中心内容不全，回滚页面样式
    
    Co-authored-by: xiu.ming <37466190+feelthehatredoftenthousandyears@users.noreply.github.com>
    Reviewed-on: http://git.cutepapertrick.cn/cutepapertrick/fate/pulls/28
    Co-authored-by: xiu.ming <code1bate@sina.com>
    Co-committed-by: xiu.ming <code1bate@sina.com>

--- apps/pantheon/src/app/[variants]/(main)/talk/features/TalkList/page.tsx
+++ apps/pantheon/src/app/[variants]/(main)/talk/features/TalkList/page.tsx
@@ -5,7 +5,7 @@ import { Divider, List, Skeleton, Tabs, type TabsProps } from 'antd';
 import { useState } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
-// import AppNavigationDrawer from '@/app/[variants]/(main)/talk/features/TopicSideMenu';
+import AppNavigationDrawer from '@/app/[variants]/(main)/talk/features/TopicSideMenu';
 // import { talkSelectors } from '@/store/talk/selectors';
 // import { useTalkStore } from '@/store/talk/store';
 import { TopicCreateInner } from '@/features/Talk/TalkCreate/topic-create-inner';
@@ -68,13 +68,13 @@ function usePaginationStatus({
     getNextPageParam: getNextPageParam,
     staleTime: 0,
     enabled: enabled,
-    select: () => undefined,
+    // select: () => undefined,
   });
 
   return {
-    hasNextPage: paginationQuery.hasNextPage,
     isFetching: paginationQuery.isFetching,
     fetchNextPage: paginationQuery.fetchNextPage,
+    paginationQuery: paginationQuery,
   };
 }
 
@@ -108,15 +108,14 @@ export default function TopicList() {
   const currentUserId = useProfile.data?.id;
   const { topicListQuery: currentListQuery } = useTopicList({ type, currentUserId });
   const topicList = currentListQuery.data || [];
-
-  const { hasNextPage: hasNextPageStatus, fetchNextPage: fetchNextPageStatus } =
+  const { paginationQuery: paginationQuery, fetchNextPage: fetchNextPageStatus } =
     usePaginationStatus({
       type: type,
       currentUserId: currentUserId,
       enabled: !isHome && !!currentUserId,
     });
-  const hasMore = isHome ? currentListQuery.hasNextPage : hasNextPageStatus;
-
+  const paginationQueryLength = paginationQuery.data?.pages[0]?.total;
+  const hasMore = isHome ? currentListQuery.hasNextPage : topicList.length < paginationQueryLength!;
   const handleNextFetch = () => {
     if (isHome) {
       currentListQuery.fetchNextPage();
@@ -168,49 +167,52 @@ export default function TopicList() {
         </>
       ),
     },
-    {
-      key: '5',
-      label: (
-        <span
-          onClick={() => {
-            handleTabChange('2');
-          }}
-        >
-          {t('talk.list.chat.mine', '我的')}
-        </span>
-      ),
-      children: (
-        <>
-          <TopicCreateInner onCreated={() => setCreateVisible(false)} open={createVisible} />
+    // {
+    //   key: '5',
+    //   label: (
+    //     <span
+    //       onClick={() => {
+    //         handleTabChange('2');
+    //       }}
+    //     >
+    //       {t('talk.list.chat.mine', '我的')}
+    //     </span>
+    //   ),
+    //   children: (
+    //     <>
+    //       <TopicCreateInner onCreated={() => setCreateVisible(false)} open={createVisible} />
 
-          <section className={styles.content_wrapper}>
-            <InfiniteScroll
-              dataLength={topicList.length}
-              endMessage={<Divider plain>{t('comment.loading.end', '没有更多了')}</Divider>}
-              hasMore={hasMore}
-              loader={<Skeleton active avatar paragraph={{ rows: 1 }} />}
-              next={handleNextFetch}
-              scrollableTarget="scrollableDiv"
-              // scrollThreshold="90%"
-            >
-              <List
-                className={styles.topic_list}
-                dataSource={topicList}
-                renderItem={(item) => (
-                  <List.Item key={item.id} style={{ padding: '0' }}>
-                    <TopicDetail id={item.id} topic={item} />
-                  </List.Item>
-                )}
-              />
-            </InfiniteScroll>
-          </section>
-        </>
-      ),
-    },
+    //       <section className={styles.content_wrapper}>
+    //         <InfiniteScroll
+    //           dataLength={topicList.length}
+    //           endMessage={<Divider plain>{t('comment.loading.end', '没有更多了')}</Divider>}
+    //           hasMore={hasMore}
+    //           loader={<Skeleton active avatar paragraph={{ rows: 1 }} />}
+    //           next={handleNextFetch}
+    //           scrollableTarget="scrollableDiv"
+    //           // scrollThreshold="90%"
+    //         >
+    //           <List
+    //             className={styles.topic_list}
+    //             dataSource={topicList}
+    //             renderItem={(item) => (
+    //               <List.Item key={item.id} style={{ padding: '0' }}>
+    //                 <TopicDetail id={item.id} topic={item} />
+    //               </List.Item>
+    //             )}
+    //           />
+    //         </InfiniteScroll>
+    //       </section>
+    //     </>
+    //   ),
+    // },
   ];
 
   return (
     <div className={styles.topic_list_box}>
+      <section className={styles.topic_side}>
+        <AppNavigationDrawer currentTab={activeTab} onTabChange={handleTabChange} />
+      </section>
       <section className={styles.topic_main} id="scrollableDiv">
         <Tabs defaultActiveKey="1" items={items} />
       </section>
